015ba45c419f8c2eba92aeaeb6730a4f6852b44c
David Corwin <dave@launchpadlab.com>
2016-07-18 13:49:37 -0500

    adding legend

--- app/app.js
+++ app/app.js
@@ -40,10 +40,104 @@ e=>end
 st->op1->e
 `
 
+const legendEntries = [{
+  title:   'Start',
+  syntax:  '<name>=>start: <text>',
+  example: 'st=>start: FooStart',
+  code:    'st=>start: FooStart\nst->'
+}, {
+  title:   'End',
+  syntax:  '<name>=>end: <text>',
+  example: 'e=>end: FooEnd',
+  code:    'e=>end: FooEnd\ne->'
+}, {
+  title:   'Operation',
+  syntax:  '<name>=>operation: <text>',
+  example: 'op=>operation: FooOperation',
+  code:    'op=>operation: FooOperation\nop->'
+}, {
+  title:   'Subroutine',
+  syntax:  '<name>=>subroutine: <text>',
+  example: 'sub=>subroutine: Subroutine',
+  code:    'sub=>subroutine: Subroutine\nsub->'
+}, {
+  title:   'Input/Output',
+  syntax:  '<name>=>inputoutput: <text>',
+  example: 'io=>inputoutput: FooInput',
+  code:    'io=>inputoutput: FooInput\nio->'
+}, {
+  title:   'Condition',
+  syntax:  '<name>=>condition: <text>',
+  example: 'cond=>condition: Yes or Foo?',
+  code:    'cond=>condition: Yes or Foo?\ncond->'
+}]
+
+const legendStyles = show => ({
+  position: 'fixed',
+  backgroundColor: '#DCDCDC',
+  width: '300px',
+  right: show ? '0' : '-300'
+})
+
+const legendToggleStyles = {
+  backgroundColor: '#DCDCDC',
+  borderRight: 'none',
+  marginLeft: '-100px',
+  marginTop: '40px',
+  width: '100px',
+  height: '40px',
+  fontSize: '24px',
+  textAlign: 'center',
+}
+
+const linkStyles = {
+  textDecoration: 'none',
+  color: 'black'
+}
+
+const LegendEntry = ({ entry }) =>
+  <div>
+    <p><b>{entry.title}</b></p>
+    <p><b>Syntax:  </b><i>{entry.syntax}</i></p>
+    <p><b>Example:  </b><i>{entry.example}</i></p>
+    <span ref={el => flowchart.parse(entry.code).drawSVG(el)}></span>
+  </div>
+
+class Legend extends React.Component {
+  constructor() {
+    super()
+    this.state = { show: false }
+  }
+
+  toggleLegend(e) {
+    e.preventDefault()
+    this.setState({ show: !this.state.show })
+  }
+
+  render() {
+    return (
+      <aside style={legendStyles(this.state.show)}>
+        <div style={legendToggleStyles}>
+          <a href="#" style={linkStyles} onClick={this.toggleLegend.bind(this)}>
+            Legend
+          </a>
+        </div>
+        {legendEntries.map((entry, idx) =>
+          <LegendEntry key={idx} entry={entry} />
+        )}
+      </aside>
+    )
+  }
+}
+
 class Flow extends React.Component {
 
   updateDiagram() {
-    flowchart.parse(this.props.diagram.plan).drawSVG(this._el)
+    try {
+      flowchart.parse(this.props.diagram.plan).drawSVG(this._el)
+    } catch(err) {
+      console.log(err)
+    }
   }
 
   componentDidMount() {
@@ -51,7 +145,7 @@ class Flow extends React.Component {
   }
 
   componentWillUpdate() {
-    this._el.removeChild(this._el.childNodes[0])
+    this._el && this._el.childNodes && this._el.childNodes[0] && this._el.removeChild(this._el.childNodes[0])
   }
 
   componentDidUpdate() {
@@ -114,8 +208,10 @@ export default class App extends React.Component {
             key={idx}
             diagram={diagram}
             idx={idx}
-            update={this.updateDiagram.bind(this, idx)} />)
-        }
+            update={this.updateDiagram.bind(this, idx)} />
+        )}
+
+        <Legend />
       </div>
     )
   }
